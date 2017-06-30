f414ae0a7d5d79e4c8aaeae0f930da05fac3844f
Zachary Jacobi <zach@alertlabs.com>
2017-06-30 10:45:29 -0400

    Found out that the all purpose insert method I was using was deprecated, so updated the insert function to make use of the insert_one and insert_many functions as appropriate, while still returning the same results.

--- SeedDB.py
+++ SeedDB.py
@@ -20,14 +20,20 @@ threads = []
 
 def insert(json_doc, collection):
     """
-    Function for inserting JSON files into a database
+    Function for inserting JSON files into a Mongo database
 
-    :param dict json_doc: A valid python dictionary, which will be converted to JSON as it is
-        inserted.
+    :param dict_or_list json_doc: A valid python dictionary or list, which will be converted to
+        the MongoDB BSON format as it is inserted into the database.
     :param str collection: The collection to insert the document into
-    :return: The unique ID given to the inserted item
+    :return: The unique ID or IDs given to the inserted item
+    :rtype: string_or_list
     """
-    return database[collection].insert(json_doc)
+    if isinstance(json_doc, dict):
+        return database[collection].insert_one(json_doc).inserted_id
+    elif len(json_doc) == 1:
+        return database[collection].insert_one(json_doc[0]).inserted_id
+    else:
+        return database[collection].insert_many(json_doc).inserted_ids
 
 
 def parse_json(json_file_path):
--- Tests/test_SeedDB.py
+++ Tests/test_SeedDB.py
@@ -52,9 +52,22 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(self.SeedDB.database.client.address, ('localhost', 27017))
 
     def test_insert(self):
-        """Ensure that the insert function works"""
-        self.assertEqual(self.SeedDB.insert({"_id": "test"}, 'test'), 'test')  # give back _id
-        self.assertEqual(self.mongo['test'].find()[0], {"_id": 'test'})  # inserted the record
+        """Ensure that the insert function works for a dictionary"""
+        self.assertEqual(self.SeedDB.insert({"_id": "test"}, 'test'), 'test')
+        self.assertEqual(self.mongo['test'].find()[0], {"_id": 'test'})
+
+    def test_insert_single_dict_in_array(self):
+        """Ensure that the insert function works for a single dict array"""
+        self.assertEqual(self.SeedDB.insert([{"_id": "test"}], 'test'), 'test')
+        self.assertEqual(self.mongo['test'].find()[0], {"_id": 'test'})
+
+    def test_insert_multi_dict_in_array(self):
+        """Ensure that the insert function works for a single dict array"""
+        ids = ['test', 'test1']
+        dicts = [{"_id": ids[0]}, {"_id": ids[1]}]
+        self.assertEqual(self.SeedDB.insert(dicts, 'test'), ids)
+        self.assertEqual(self.mongo['test'].find()[0], dicts[0])
+        self.assertEqual(self.mongo['test'].find()[1], dicts[1])
 
     def test_worker(self):
         """Ensure that the worker function can insert JSON into the database"""
@@ -64,4 +77,4 @@ class TestDatabase(unittest.TestCase):
         self.SeedDB.q.join()
         self.SeedDB.q.put(None)
         t.join()
-        self.assertEqual(self.mongo['test'].find()[0], {"_id": 'test'})  # inserted the record
+        self.assertEqual(self.mongo['test'].find()[0], {"_id": 'test'})
