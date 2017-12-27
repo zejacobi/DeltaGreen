5444c262093f86fe33470d523d3e17a5a07fade5
Zachary Jacobi <zach@alertlabs.com>
2017-12-27 11:09:54 -0500

    Added in option to make the server mobile friendly but removed it as the default; for some reason using 0.0.0.0 instead of localhost turns off jade reloading???

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 from flask import Flask
 from API.v1 import V1
 from templates.Views import Views
@@ -8,6 +10,10 @@ app.register_blueprint(V1, url_prefix='/api/v1')
 app.register_blueprint(Views, url_prefix='')
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
+host = 'localhost'
+
+if argv and len(argv) > 1 and argv[1] == 'mobile':
+    host = '0.0.0.0'
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=8080, debug=False)
+    app.run(host, port=8080, debug=False)
