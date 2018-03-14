6d92bf57ad43ca568e35e756f66769312043b159
Zachary Jacobi <zach@alertlabs.com>
2018-03-14 19:34:50 -0400

    Adds in support for LetsEncrypt acme-challenge verification

---
+++ LetsEncryptConfig.py
@@ -0,0 +1,3 @@
+ROOT = '/.well-known/acme-challenge'
+ENDPOINT = '/k9s7WeOPg3HdSjwlAqEVRxnezsGGe-CFOwPfOcU3VgU'
+RESPONSE = 'k9s7WeOPg3HdSjwlAqEVRxnezsGGe-CFOwPfOcU3VgU.QBkCfzPq0mKXIJSktgl4_b7psKazh3MSZ8juWnZbJbg'
